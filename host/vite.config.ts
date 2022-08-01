10f22ec5deeb0efe4991dda43539a0fbe3fc99e5
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2022-08-02 04:15:17 +0530

    Make react and react-dom singletons

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
+import pkg from "./package.json";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
@@ -15,7 +17,17 @@ export default defineConfig({
       remotes: {
         remote: "http://localhost:8082/assets/remoteEntry.js",
       },
-      shared: ["react", "react-dom"],
+      shared: {
+        ...pkg.dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: pkg.dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: pkg.dependencies["react-dom"],
+        },
+      },
     }),
   ],
   build: {
--- remote/vite.config.ts
+++ remote/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
+import pkg from "./package.json";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
@@ -16,7 +18,17 @@ export default defineConfig({
       exposes: {
         "./App": "./src/App.tsx",
       },
-      shared: ["react", "react-dom"],
+      shared: {
+        ...pkg.dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: pkg.dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: pkg.dependencies["react-dom"],
+        },
+      },
     }),
   ],
   build: {
